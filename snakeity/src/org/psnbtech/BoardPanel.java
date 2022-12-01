b0a6e55d86d413dc5edb187dc87e692ea72d0f15
Annika Clewis <71907192+annikaClew@users.noreply.github.com>
2022-12-01 15:31:46 -0800

    Update BoardPanel.java

--- snakeity/src/org/psnbtech/BoardPanel.java
+++ snakeity/src/org/psnbtech/BoardPanel.java
@@ -7,7 +7,7 @@ import java.awt.Graphics;
 import java.awt.Point;
 
 import javax.swing.JPanel;
-
+//testing hehe
 /**
  * The {@code BoardPanel} class is responsible for managing and displaying the
  * contents of the game board.
