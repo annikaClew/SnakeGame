29cd9cfbe5f6183456a59a28e72c28b14968133d
Annika Clewis <71907192+annikaClew@users.noreply.github.com>
2022-12-05 17:18:11 -0800

    Add files via upload

---
+++ src/org/psnbtech/SidePanelTest.java
@@ -0,0 +1,33 @@
+package org.psnbtech;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+import static org.junit.Assert.*;
+
+public class SidePanelTest {
+    Graphics g;
+    public SnakeGame game;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void paintComponent() {
+        SidePanel sp = new SidePanel(game);
+    	SnakeGame g = new SnakeGame();
+    	g.startGame();
+    	KeyEvent key = new KeyEvent(g, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,  KeyEvent.VK_ENTER);
+        g.getKeyListeners()[0].keyPressed(key);
+        
+    }
+}
